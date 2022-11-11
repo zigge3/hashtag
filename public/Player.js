c9338a3869c7315fddcb203adbcebee5cf92eb4c
Timmy Ylander <timmy.ylander@gmail.com>
2022-11-11 21:23:53 +0100

    adds max velocity

--- public/Player.js
+++ public/Player.js
@@ -8,8 +8,7 @@ export default class Player {
   update = ({ objects, world, delta }) => {
     const [velX, velY] = this.velocity;
     const [gravX, gravY] = world.gravity.map((x) => x * (delta / 1000));
-    this.velocity = [velX + gravX, velY + gravY];
-
+    this.velocity = [velX + gravX, Math.min(velY + gravY, 7)];
     const [posX, posY] = this.position;
     const newPos = [posX + velX, posY + velY];
     const collisionFound = [...objects, ...world.objects]
@@ -26,11 +25,17 @@ export default class Player {
 
         return acc;
       }, []);
-    console.log(collisionFound);
 
     if (!collisionFound.length) {
       this.position = newPos;
     } else {
+      const { x, y } = this.distance(
+        [...this.position, ...this.size],
+        [...collisionFound[0].position, ...collisionFound[0].size]
+      );
+      if (Math.abs(y) < 7.1) {
+        this.velocity = [this.velocity[0], 0];
+      }
     }
   };
 
@@ -42,10 +47,23 @@ export default class Player {
     return col;
   };
 
+  distance = (posA, posB) => {
+    const [r1X, r1Y, r1W, r1H] = posA;
+    const [r2X, r2Y, r2W, r2H] = posB;
+    return { x: r1X + r1W - (r2X + r2W), y: r1Y + r1H - r2Y };
+  };
+
   draw = ({ ctx }) => {
     ctx.beginPath();
     ctx.strokeStyle = "white";
     ctx.rect(...this.position, ...this.size);
     ctx.stroke();
+
+    ctx.fillStyle = "white";
+    ctx.font = "20px serif";
+    ctx.fillText(
+      `x: ${this.velocity[0]} y: ${Math.round(this.velocity[1] * 100) / 100}`,
+      ...this.position
+    );
   };
 }
