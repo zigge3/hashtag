1f4364dcb267203f32847b8c175f36ee188fdb2d
Timmy Ylander <timmy.ylander@gmail.com>
2022-11-20 20:27:39 +0100

    Adds world sync

---
+++ public/worlds/standard.js
@@ -0,0 +1,28 @@
+import WorldObject from "../WorldObject";
+export default [
+  new WorldObject({
+    id: Math.random() * 100,
+    position: [0, 1000],
+    size: [1200, 100],
+    isStatic: true,
+  }),
+  new WorldObject({
+    id: Math.random() * 100,
+    position: [-50, 0],
+    size: [100, 1000],
+    isStatic: true,
+  }),
+  new WorldObject({
+    id: Math.random() * 100,
+    position: [1150, 0],
+    size: [100, 1000],
+    isStatic: true,
+  }),
+  new WorldObject({
+    id: Math.random() * 100,
+    position: [500, 500],
+    textureName: "dick.png",
+    size: [100, 100],
+    isStatic: true,
+  }),
+];
